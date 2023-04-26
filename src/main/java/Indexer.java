56d122eefea7bc0e538f6095a7896234080cf95b
Doris Peter <doris.peter@partner.bmw.com>
2023-04-26 08:02:42 +0200

    initial commit

--- src/main/java/Indexer.java
+++ src/main/java/Indexer.java
@@ -1,8 +1,3 @@
-import java.io.IOException;
-import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.Http2SolrClient;
-import org.apache.solr.common.SolrInputDocument;
 
 /**
  * @author Doris Peter, IDV AG
@@ -10,35 +5,6 @@ import org.apache.solr.common.SolrInputDocument;
 public class Indexer {
 
   public static void main(String[] args) {
-    try (SolrClient client =
-        new Http2SolrClient.Builder("http://localhost:8983/solr/library").build()) {
-      SolrInputDocument doc = new SolrInputDocument();
-      doc.addField("id", "8");
-      doc.addField("isbn", "978-3596271207");
-      doc.addField("title", "Endstation Sehnsucht");
-      doc.addField(
-          "description",
-          "Endstation Sehnsucht‹ erzählt die Geschichte von Blanche Dubois, "
-              + "einer Lehrerin aus den Südstaaten. Blanche erlebt die Auflösung ihrer Familie, "
-              + "einer nach dem anderen stirbt, sie muß zusehen, wie der einstmals stolze Besitz, "
-              + "das Herrenhaus »Belle Reve«, zwangsversteigert wird.");
-      doc.addField("author", "Tennessee Williams");
-      doc.addField("country", "USA");
-      doc.addField("year", "2010");
 
-      try {
-        client.add(doc);
-      } catch (SolrServerException | IOException e) {
-        throw new RuntimeException(e);
-      }
-      try {
-        client.commit();
-      } catch (SolrServerException | IOException e) {
-        throw new RuntimeException(e);
-      }
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    System.out.println("Commit done");
   }
 }
--- src/main/java/Searcher.java
+++ src/main/java/Searcher.java
@@ -1,42 +1,7 @@
-import java.io.IOException;
-import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.Http2SolrClient;
-import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocument;
-import org.apache.solr.common.SolrDocumentList;
 
 /**
  * @author Doris Peter, IDV AG
  */
 public class Searcher {
-  private final SolrClient client;
 
-  public Searcher(){
-    String solrUrl = "http://localhost:8983/solr/library";
-    client =
-        new Http2SolrClient.Builder(solrUrl).build();
-  }
-
-  public QueryResponse runQuery(SolrQuery query) {
-    QueryResponse response;
-    try {
-      response = client.query(query);
-    } catch (SolrServerException | IOException e) {
-      throw new RuntimeException(e);
-    }
-    SolrDocumentList results = response.getResults();
-    if (results.size() == 0) {
-      System.out.println("Sorry, no matching documents were found.");
-      for (SolrDocument result : results) {
-        System.out.println(result);
-      }
-    } else {
-      for (SolrDocument result : results) {
-        System.out.println(result);
-      }
-    }
-    return response;
-  }
 }
